868bfc030176b70254338674b5b70adb07e633be
HarryRamirez <kennyharry47@gmail.com>
2021-10-01 18:57:23 -0500

    conexion base de datos

---
+++ src/main/java/Modelos/Proveedor.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelos;
+
+/**
+ *
+ * @author kenny
+ */
+public class Proveedor {
+   int idProveedor;
+   String nombre;
+   String  nit;
+
+    public Proveedor() {
+    }
+
+    public int getIdProveedor() {
+        return idProveedor;
+    }
+
+    public void setIdProveedor(int idProveedor) {
+        this.idProveedor = idProveedor;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNit() {
+        return nit;
+    }
+
+    public void setNit(String nit) {
+        this.nit = nit;
+    }
+   public void crearProveedor(){
+   
+   }
+   public void consultarProveedor(){
+   
+   }
+   public void listarProveedor(){
+   
+   }
+   public void actualizarProveedor(){
+   
+   }
+   public void eliminarProveedor(){
+   
+   }
+}
